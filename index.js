6a6c374e24ee7839575f25c252e443ab5dda58bf
Benta <bentaakitwi500@gmail.com>
2023-03-20 00:36:44 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution here
+function findMatching(drivers, string){
+    return drivers.filter(function(driver) {return driver.toLowerCase() ===string.toLowerCase() })
+}
+function fuzzyMatch(drivers, string){
+    return drivers.filter(function(driver) {return driver.toLowerCase().substring(0, string.length) === string.toLowerCase() })
+}
+function matchName(driver, argument){
+    return driver.filter(function (driver) {return driver.name === argument })
+}
